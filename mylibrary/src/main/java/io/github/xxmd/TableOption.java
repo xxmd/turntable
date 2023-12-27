13a26d4ae8d31ef767436894de7859c5bad2cd7b
GMJ <410515742@qq.com>
2023-12-27 09:36:21 +0800

    TableOption attr default value

--- mylibrary/src/main/java/io/github/xxmd/TableOption.java
+++ mylibrary/src/main/java/io/github/xxmd/TableOption.java
@@ -1,13 +1,15 @@
 package io.github.xxmd;
 
+import android.graphics.Color;
+
 import androidx.annotation.ColorInt;
 
 public class TableOption<T> {
     public String label;
     public T value;
-    public int weight;
+    public int weight = 1;
     @ColorInt
-    public int labelColor;
+    public int labelColor = Color.BLACK;
     @ColorInt
     public int backgroundColor;
 }
